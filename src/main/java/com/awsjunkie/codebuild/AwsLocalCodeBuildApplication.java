55723511ecb67c52cbd4e8688707f5af5e4d575c
AwsJunkie <100114557+awsjunkie@users.noreply.github.com>
2023-07-15 18:23:52 -0400

    Add files via upload

---
+++ src/main/java/com/awsjunkie/codebuild/AwsLocalCodeBuildApplication.java
@@ -0,0 +1,13 @@
+package com.awsjunkie.codebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLocalCodeBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLocalCodeBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awsjunkie/codebuild/AwsLocalCodeBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsjunkie.codebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLocalCodeBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
